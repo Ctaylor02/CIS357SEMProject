b9e8849140188ed0160168c2e07130308daffb82
Sam Uptigrove <samuptigrove@SamsLaptop.local>
2025-10-12 21:04:59 -0400

    Added files for starter app

---
+++ CIS357Project/CIS357ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIS357ProjectApp.swift
+//  CIS357Project
+//
+//  Created by Sam Uptigrove on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CIS357ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CIS357ProjectTests/CIS357ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CIS357ProjectTests.swift
+//  CIS357ProjectTests
+//
+//  Created by Sam Uptigrove on 10/12/25.
+//
+
+import Testing
+@testable import CIS357Project
+
+struct CIS357ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
